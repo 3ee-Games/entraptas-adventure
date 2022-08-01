ae8b00716aeff38eaa8ffec7dc0af1cb35846867
Ryan Sadwick <rsadwick@gmail.com>
2022-07-31 23:16:33 -0400

    inital commit for game

---
+++ src/Player.gd
@@ -0,0 +1,54 @@
+extends KinematicBody
+
+export var speed = 14
+export var move_speed = 20;
+export var jump_power = 50;
+export var fall_acceleration = 75
+export var jump_impulse = 20
+
+var moving_anim_name = "walking";
+var idle_anim_name = "idle";
+var velocity = Vector3(0, 0, 0);
+var moving = false
+var camera
+
+func _ready():
+	$Pivot/Character.play(idle_anim_name)
+
+func _physics_process(delta):
+	camera = get_node("Target/Camera").get_global_transform()
+	if (Input.is_action_pressed("move_right")):
+		velocity.x = move_speed;
+		moving = true
+	elif (Input.is_action_pressed("move_left")):
+		velocity.x = -move_speed;
+		moving = true
+	else:
+		velocity.x = 0;
+		moving = false
+	
+	if (Input.is_action_pressed("move_down")):
+		velocity.z = move_speed;
+		moving = true
+	elif (Input.is_action_pressed("move_up")):
+		velocity.z = -move_speed;
+		moving = true
+	else:
+		velocity.z = 0;
+		moving = false
+		
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		velocity.y += jump_impulse
+		moving = true
+		
+	velocity.y -= fall_acceleration * delta
+	velocity = move_and_slide(velocity, Vector3.UP)
+
+	global_translate(velocity * delta)
+	if (moving):
+		$Pivot/Character.play(moving_anim_name);
+	else:
+		$Pivot/Character.play(idle_anim_name);
+		
+	moving = false
+
